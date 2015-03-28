2deaba69ae85785fd8ad9fbcddd5fd91c9fee834
cjang5 <chrismangggg@gmail.com>
2015-03-28 00:51:08 -0400

    Fucking finally got the actionBar to fuck off

--- Snapchatkiller/app/src/main/java/com/chrisjang/snapchatkiller/TitleScreen.java
+++ Snapchatkiller/app/src/main/java/com/chrisjang/snapchatkiller/TitleScreen.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class TitleScreen extends ActionBarActivity {
@@ -11,7 +12,12 @@ public class TitleScreen extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Hide the actionBar for the Title Screen
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_title_screen);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 
 
